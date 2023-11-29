8e568aa9e0fb819834786b425cadcb724cc726a5
Chris Laprun <claprun@redhat.com>
2023-11-29 10:20:29 +0100

    refactor: rename file more appropriately

--- server/src/test/java/io/github/metacosm/power/sensors/macos/powermetrics/MacOSPowermetricsSensorTest.java
+++ server/src/test/java/io/github/metacosm/power/sensors/macos/powermetrics/MacOSPowermetricsSensorTest.java
@@ -19,7 +19,7 @@ class MacOSPowermetricsSensorTest {
         final var pid1 = sensor.register(29419);
         final var pid2 = sensor.register(391);
         final var measure = sensor
-                .extractPowerMeasure(Thread.currentThread().getContextClassLoader().getResourceAsStream("foo.txt"));
+                .extractPowerMeasure(Thread.currentThread().getContextClassLoader().getResourceAsStream("sonoma-m1max.txt"));
         final var metadata = sensor.metadata();
         assertEquals(((23.88 / 1222.65) * 211), measure.get(pid1)[metadata.metadataFor(MacOSPowermetricsSensor.CPU).index()]);
         assertEquals(((283.25 / 1222.65) * 211), measure.get(pid2)[metadata.metadataFor(MacOSPowermetricsSensor.CPU).index()]);
