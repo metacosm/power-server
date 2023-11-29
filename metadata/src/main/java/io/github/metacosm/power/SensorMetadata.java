1499b6767276a4344cd3e514c9e440088bfdce94
Chris Laprun <claprun@redhat.com>
2023-11-29 17:55:26 +0100

    fix: make SensorMetadata deserializable

--- metadata/src/main/java/io/github/metacosm/power/SensorMetadata.java
+++ metadata/src/main/java/io/github/metacosm/power/SensorMetadata.java
@@ -1,12 +1,15 @@
 package io.github.metacosm.power;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Collections;
 import java.util.Map;
 
 public class SensorMetadata {
     public record ComponentMetadata(String name, int index, String description, boolean isAttributed, String unit){};
 
+    @JsonCreator
     public SensorMetadata(Map<String, ComponentMetadata> components) {
         this.components = components;
     }
@@ -24,5 +27,9 @@ public class SensorMetadata {
 
     public int componentCardinality() {
         return components.size();
+    }
+
+    public Map<String, ComponentMetadata> components() {
+        return Collections.unmodifiableMap(components);
     }
 }
