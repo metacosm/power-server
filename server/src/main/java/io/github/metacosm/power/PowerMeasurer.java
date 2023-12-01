b0b41b1a432490390cc789adf5611933e45936f2
Chris Laprun <claprun@redhat.com>
2023-12-01 22:23:52 +0100

    fix: broadcast ticks to all subscribers, run subscriptions on worker pool

--- server/src/main/java/io/github/metacosm/power/PowerMeasurer.java
+++ server/src/main/java/io/github/metacosm/power/PowerMeasurer.java
@@ -3,14 +3,12 @@ package io.github.metacosm.power;
 import io.github.metacosm.power.sensors.PowerSensor;
 import io.github.metacosm.power.sensors.RegisteredPID;
 import io.smallrye.mutiny.Multi;
-import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.infrastructure.Infrastructure;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
 import java.time.Duration;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @ApplicationScoped
 public class PowerMeasurer {
@@ -18,7 +16,7 @@ public class PowerMeasurer {
     public static final int SAMPLING_FREQUENCY_IN_MILLIS = 500;
     @Inject
     PowerSensor sensor;
-    
+
     private Multi<Map<RegisteredPID, double[]>> periodicSensorCheck;
 
     public Multi<double[]> startTracking(String pid) throws Exception {
@@ -26,12 +24,13 @@ public class PowerMeasurer {
         final var parsedPID = Long.parseLong(pid);
         ProcessHandle.of(parsedPID).orElseThrow(() -> new IllegalArgumentException("Unknown process: " + pid));
 
-        if(!sensor.isStarted()) {
+        if (!sensor.isStarted()) {
             sensor.start(SAMPLING_FREQUENCY_IN_MILLIS);
             periodicSensorCheck = Multi.createFrom().ticks()
                     .every(Duration.ofMillis(SAMPLING_FREQUENCY_IN_MILLIS))
-                    .emitOn(Infrastructure.getDefaultWorkerPool())
-                    .map(sensor::update);
+                    .map(sensor::update)
+                    .broadcast().toAllSubscribers()
+                    .runSubscriptionOn(Infrastructure.getDefaultWorkerPool());
         }
         final var registeredPID = sensor.register(parsedPID);
         return periodicSensorCheck.map(registeredPIDMap -> registeredPIDMap.get(registeredPID))
