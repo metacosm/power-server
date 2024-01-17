51fa3ba7be848caaa6c96f2f0f1c9771d8775679
Chris Laprun <metacosm@users.noreply.github.com>
2024-01-17 20:57:26 +0100

    test: make tests arch-specific (#16)

--- server/src/test/java/io/github/metacosm/PowerResourceTest.java
+++ server/src/test/java/io/github/metacosm/PowerResourceTest.java
@@ -3,6 +3,7 @@ package io.github.metacosm;
 import io.github.metacosm.power.SensorMetadata;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 
@@ -25,7 +26,8 @@ public class PowerResourceTest {
 
     @Test
     @EnabledOnOs(OS.MAC)
-    public void testMacOSMetadataEndpoint() {
+    @EnabledIfSystemProperty(named = "os.arch", matches = "aarch64")
+    public void testMacOSAppleSiliconMetadataEndpoint() {
         final var metadata = given()
                 .when().get("/power/metadata")
                 .then()
@@ -39,6 +41,31 @@ public class PowerResourceTest {
         assertEquals(0, cpu.index());
         assertEquals("CPU", cpu.name());
         assertEquals("mW", cpu.unit());
+        assertTrue(cpu.isAttributed());
+
+        final var cpuShare = metadata.metadataFor("cpuShare");
+        assertEquals(3, cpuShare.index());
+        assertEquals("cpuShare", cpuShare.name());
+        assertEquals("decimal percentage", cpuShare.unit());
+        assertFalse(cpuShare.isAttributed());
+    }
+
+    @Test
+    @EnabledOnOs(OS.MAC)
+    @EnabledIfSystemProperty(named = "os.arch", matches = "x86_64")
+    public void testMacOSIntelMetadataEndpoint() {
+        final var metadata = given()
+                .when().get("/power/metadata")
+                .then()
+                .statusCode(200)
+                .extract().body().as(SensorMetadata.class);
+        assertEquals(2, metadata.componentCardinality());
+        assertTrue(metadata.documentation().contains("powermetrics"));
+        assertTrue(metadata.components().keySet().containsAll(Set.of("Package", "cpuShare")));
+
+        final var cpu = metadata.metadataFor("Package");
+        assertEquals(0, cpu.index());
+        assertEquals("W", cpu.unit());
         assertTrue(cpu.isAttributed());
 
         final var cpuShare = metadata.metadataFor("cpuShare");
