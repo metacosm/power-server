f0a9de57d17930f1cde857793fb8818b1bf4297f
Chris Laprun <claprun@redhat.com>
2024-07-06 20:22:14 +0200

    feat: attempt to detect lack of sudo access better

--- server/src/main/java/net/laprun/sustainability/power/sensors/macos/powermetrics/ProcessMacOSPowermetricsSensor.java
+++ server/src/main/java/net/laprun/sustainability/power/sensors/macos/powermetrics/ProcessMacOSPowermetricsSensor.java
@@ -9,11 +9,25 @@ public class ProcessMacOSPowermetricsSensor extends MacOSPowermetricsSensor {
         // extract metadata
         try {
             final var exec = new ProcessBuilder()
-                    .command("sudo", "powermetrics", "--samplers", "cpu_power", "-i", "10", "-n", "1")
+                    .command("powermetrics", "--samplers", "cpu_power", "-i", "10", "-n", "1")
                     .start();
+
+            // if the process is already dead, get the error
+            if (!exec.isAlive()) {
+                final var exitValue = exec.exitValue();
+                if (exitValue != 0) {
+                    final String errorMsg;
+                    try (final var error = exec.errorReader()) {
+                        errorMsg = error.readLine();
+                    }
+                    throw new RuntimeException(
+                            "Couldn't execute powermetrics. Error code: " + exitValue + ", message: " + errorMsg);
+                }
+            }
+
             initMetadata(exec.getInputStream());
         } catch (Exception e) {
-            throw new RuntimeException("Couldn't execute powermetrics to extract metadata", e);
+            throw new RuntimeException("Couldn't extract sensor metadata", e);
         }
     }
 
@@ -21,7 +35,7 @@ public class ProcessMacOSPowermetricsSensor extends MacOSPowermetricsSensor {
         if (!isStarted()) {
             // it takes some time for the external process in addition to the sampling time so adjust the sampling frequency to account for this so that at most one measure occurs during the sampling time window
             final var freq = Long.toString(frequency - 50);
-            powermetrics = new ProcessBuilder().command("sudo", "powermetrics", "--samplers", "cpu_power,tasks",
+            powermetrics = new ProcessBuilder().command("powermetrics", "--samplers", "cpu_power,tasks",
                     "--show-process-samp-norm", "--show-process-gpu", "-i", freq).start();
         }
     }
