73a5a1c3fc9855c7580cfa7e1e65c351af82bf17
Chris Laprun <claprun@redhat.com>
2025-01-28 10:17:42 +0100

    feat: display metadata / configuration on start

--- server/src/main/java/net/laprun/sustainability/power/PowerResource.java
+++ server/src/main/java/net/laprun/sustainability/power/PowerResource.java
@@ -2,18 +2,26 @@ package net.laprun.sustainability.power;
 
 import java.time.Duration;
 
+import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 import org.jboss.resteasy.reactive.RestStreamElementType;
 
+import io.quarkus.logging.Log;
+import io.quarkus.runtime.StartupEvent;
 import io.smallrye.mutiny.Multi;
 
 @Path("/power")
 public class PowerResource {
     @Inject
     PowerMeasurer measurer;
+
+    public void onStartup(@Observes StartupEvent event) {
+        Log.info("\nConfigured sampling period: " + samplingPeriod() +
+                "\nDetected metadata:\n" + metadata());
+    }
 
     @GET
     @RestStreamElementType(MediaType.APPLICATION_JSON)
