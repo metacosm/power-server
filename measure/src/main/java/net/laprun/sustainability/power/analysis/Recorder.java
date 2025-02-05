0f59ba2a2a80047a54e337dd9bc778da7bd7c12c
Chris Laprun <claprun@redhat.com>
2025-02-05 14:49:39 +0100

    feat: add access to actual measures on Recorder

--- measure/src/main/java/net/laprun/sustainability/power/analysis/Recorder.java
+++ measure/src/main/java/net/laprun/sustainability/power/analysis/Recorder.java
@@ -3,5 +3,9 @@ package net.laprun.sustainability.power.analysis;
 import java.util.stream.DoubleStream;
 
 public interface Recorder {
-    DoubleStream measures();
+    default DoubleStream measures() {
+        return DoubleStream.of(liveMeasures());
+    }
+
+    double[] liveMeasures();
 }
