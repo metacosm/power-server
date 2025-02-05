6fcb6be3ad60fac1817020417c485467a27cd442
Chris Laprun <claprun@redhat.com>
2025-02-05 14:57:03 +0100

    refactor: rename PartialCursor to simply Cursor

--- measure/src/main/java/net/laprun/sustainability/power/measure/MeasureBackedCursor.java
+++ measure/src/main/java/net/laprun/sustainability/power/measure/MeasureBackedCursor.java
@@ -2,7 +2,7 @@ package net.laprun.sustainability.power.measure;
 
 import net.laprun.sustainability.power.analysis.Recorder;
 
-public record MeasureBackedCursor(Recorder measure, PartialCursor cursor) {
+public record MeasureBackedCursor(Recorder measure, Cursor cursor) {
     public double sum() {
         return cursor.sum(measure.liveMeasures());
     }
--- measure/src/main/java/net/laprun/sustainability/power/measure/Timing.java
+++ measure/src/main/java/net/laprun/sustainability/power/measure/Timing.java
@@ -3,7 +3,7 @@ package net.laprun.sustainability.power.measure;
 import java.time.Duration;
 
 public record Timing(long[] timestamps, long startedAt, long samplePeriod) {
-    public PartialCursor cursorOver(long timestamp, Duration duration) {
+    public Cursor cursorOver(long timestamp, Duration duration) {
         return Cursors.cursorOver(timestamps, timestamp, duration, startedAt, samplePeriod);
     }
 }
