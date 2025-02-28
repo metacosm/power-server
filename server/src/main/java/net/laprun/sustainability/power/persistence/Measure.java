32f00b698c06f1cc5ee81dde0056a679ed88a75c
Chris Laprun <claprun@redhat.com>
2025-02-28 19:17:27 +0100

    wip: initial work on persisting measures to SQLite

---
+++ server/src/main/java/net/laprun/sustainability/power/persistence/Measure.java
@@ -0,0 +1,19 @@
+package net.laprun.sustainability.power.persistence;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+@Entity
+public class Measure extends PanacheEntity {
+    public long pid;
+    public long startTime;
+    public long endTime;
+    public double[] components;
+
+    public static List<Measure> forPID(long pid) {
+        return find("pid", pid).list();
+    }
+}
