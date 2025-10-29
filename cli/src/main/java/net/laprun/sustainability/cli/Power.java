262cf66fe55c11561d190464ac858322877166f6
Chris Laprun <metacosm@gmail.com>
2025-10-29 19:20:57 +0100

    feat: implement DB-backed energy measurement

--- cli/src/main/java/net/laprun/sustainability/cli/Power.java
+++ cli/src/main/java/net/laprun/sustainability/cli/Power.java
@@ -2,16 +2,18 @@ package net.laprun.sustainability.cli;
 
 import java.io.IOException;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+import com.zaxxer.nuprocess.NuProcess;
+import com.zaxxer.nuprocess.NuProcessBuilder;
 
 import io.quarkus.logging.Log;
 import io.quarkus.runtime.Quarkus;
-import io.smallrye.mutiny.Uni;
-import io.smallrye.mutiny.infrastructure.Infrastructure;
-import io.smallrye.mutiny.subscription.Cancellable;
 import net.laprun.sustainability.power.Measure;
-import net.laprun.sustainability.power.Security;
 import net.laprun.sustainability.power.SensorUnit;
 import net.laprun.sustainability.power.analysis.total.TotalSyntheticComponent;
+import net.laprun.sustainability.power.nuprocess.BaseProcessHandler;
+import net.laprun.sustainability.power.persistence.Persistence;
 import picocli.CommandLine;
 
 @CommandLine.Command
@@ -28,88 +30,102 @@ public class Power implements Runnable {
             "--command" }, required = true, description = "Command to measure energy consumption for")
     String cmd;
 
-    PowerMeasurer measurer;
-    Security security;
+    private final PowerMeasurer measurer;
 
-    public Power(PowerMeasurer measurer, Security security) throws IOException {
+    public Power(PowerMeasurer measurer) throws IOException {
         this.measurer = measurer;
-        this.security = security;
     }
 
     @Override
     public void run() {
-        // start powermetrics
-        final Cancellable powermetrics;
-        try {
-            powermetrics = measurer.startTrackingApp("kernel task", 1, "ignore");
-        } catch (Exception e) {
-            Log.error("Error starting kernel task", e);
-            throw new RuntimeException(e);
+        if (cmd == null || cmd.isEmpty()) {
+            Log.info("No command specified, exiting.");
+            return;
         }
 
-        // run target command to completion
-        final var cmdPid = Uni.createFrom()
-                .item(this::runCommandToMeasureToCompletion)
-                .runSubscriptionOn(Infrastructure.getDefaultWorkerPool())
-                .await()
-                .indefinitely();
+        name = name == null ? String.join(" ", cmd) : name;
+        session = session == null ? Persistence.defaultSession(name) : session;
 
-        final var measure = extractPowerConsumption(cmdPid);
-        Log.infof("Power consumption: %s", measure);
+        try {
+            measurer.start(session);
 
-        powermetrics.cancel();
-        measurer.stop();
+            final var commandHandler = new ExternalProcessHandler(cmd);
+            final var command = new NuProcessBuilder(commandHandler, commandHandler.command());
+            final var process = command.start();
+
+            final var pid = process.getPID();
+            Log.infof("Recording energy consumption for application '%s' (pid: %d) with session '%s'", name, pid,
+                    session);
+            measurer.startTrackingApp(name, pid, session);
+
+            process.waitFor(60, TimeUnit.SECONDS);
+
+            final var measureTime = measurer.persistence()
+                    .synthesizeAndAggregateForSession(name, session, m -> (double) m.duration())
+                    .orElseThrow(() -> new RuntimeException("Could not compute measure duration"));
+            final var appPower = extractPowerConsumption(name);
+            final var systemPower = extractPowerConsumption(Persistence.SYSTEM_TOTAL_APP_NAME);
+            Log.infof("Command ran for: %dms, measure time: %3fms", commandHandler.duration(), measureTime);
+            Log.infof("App '%s' power consumption: %3.2f%s", cmd, appPower.value(), appPower.unit());
+            Log.infof("Total system power consumption: %3.2f%s", systemPower.value(), systemPower.unit());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            measurer.stop();
+        }
+
         Quarkus.waitForExit();
     }
 
-    private Measure extractPowerConsumption(long pid) {
+    private Measure extractPowerConsumption(String applicationName) {
         // first read metadata
         final var metadata = measurer.metadata();
-        Log.infof("Metadata:\n%s", metadata);
 
         // create a synthetic component to get the total power
-        final var totalPower = new TotalSyntheticComponent(metadata, SensorUnit.W, 0, 1, 2);
+        final var totaler = new TotalSyntheticComponent(metadata, SensorUnit.W, 0, 1, 2);
 
-        final var power = measurer.persistence()
-                .synthesizeAndAggregateForSession(name, session, m -> totalPower.synthesizeFrom(m.components, m.startTime))
-                .map(measure -> new Measure(measure, totalPower.metadata().unit()))
+        final var appPower = measurer.persistence()
+                .synthesizeAndAggregateForSession(applicationName, session,
+                        m -> totaler.synthesizeFrom(m.components, m.startTime))
+                .map(measure -> new Measure(measure, totaler.metadata().unit()))
                 .orElseThrow(() -> new RuntimeException("Could not extract power consumption"));
 
         Quarkus.asyncExit();
-        return power;
+        return appPower;
     }
 
-    private Process runCommandToMeasure() {
-        final var command = new ProcessBuilder().command("/bin/bash", "-c", stripped(cmd).orElseThrow());
-        try {
-            final var process = command.start();
-            name = name == null ? String.join(" ", cmd) : name;
-            session = session == null ? "" + System.currentTimeMillis() : session;
-            Log.infof("Recording energy consumption for application '%s' (pid: %d) with session '%s'", name, process.pid(),
-                    session);
-            measurer.startTrackingApp(name, process.pid(), session);
-            return process;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+    private class ExternalProcessHandler extends BaseProcessHandler {
+        private long startTime;
+        private long duration;
+
+        public ExternalProcessHandler(String cmd) {
+            super("/bin/bash", "-c", stripped(cmd).orElseThrow());
         }
-    }
 
-    private long runCommandToMeasureToCompletion() {
-        final var process = runCommandToMeasure();
-        try {
-            process.waitFor();
-            security.onNonZeroExitCode(process,
-                    (exitCode, errorMsg) -> Log.errorf("Couldn't run command %s, got exit code: %d, reason: %s",
-                            process.info().commandLine(), exitCode, errorMsg));
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        @Override
+        public void onStart(NuProcess nuProcess) {
+            super.onStart(nuProcess);
+            this.startTime = System.nanoTime();
         }
-        return process.pid();
-    }
 
-    private static Optional<String> stripped(String s) {
-        return Optional.ofNullable(s)
-                .map(String::strip)
-                .filter(s2 -> !s2.isBlank());
+        @Override
+        public void onExit(int statusCode) {
+            try {
+                super.onExit(statusCode);
+            } finally {
+                duration = System.nanoTime() - this.startTime;
+                measurer.stop();
+            }
+        }
+
+        public long duration() {
+            return duration / 1_000_000;
+        }
+
+        private static Optional<String> stripped(String s) {
+            return Optional.ofNullable(s)
+                    .map(String::strip)
+                    .filter(s2 -> !s2.isBlank());
+        }
     }
 }
