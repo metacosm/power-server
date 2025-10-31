ec5b561a3afde52f7f3e7821f9c873e9d46ebaa1
Chris Laprun <metacosm@gmail.com>
2025-10-31 13:44:01 +0100

    feat: add ProcessUtils

---
+++ metadata/src/main/java/net/laprun/sustainability/power/ProcessUtils.java
@@ -0,0 +1,26 @@
+package net.laprun.sustainability.power;
+
+import java.time.Duration;
+import java.util.Optional;
+
+public enum ProcessUtils {
+    ;
+
+    public static long validPIDOrFail(String pid) {
+        final var parsedPID = Long.parseLong(pid);
+        processHandleOf(parsedPID);
+        return parsedPID;
+    }
+
+    public static Optional<Duration> cpuUsageOf(long pid) {
+        return cpuUsageOf(processHandleOf(pid));
+    }
+
+    public static Optional<Duration> cpuUsageOf(ProcessHandle ph) {
+        return ph.info().totalCpuDuration();
+    }
+
+    public static ProcessHandle processHandleOf(long parsedPID) {
+        return ProcessHandle.of(parsedPID).orElseThrow(() -> new IllegalArgumentException("Unknown process: " + parsedPID));
+    }
+}
