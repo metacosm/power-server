98dcba1ade6649b3de2c78b2bc462f4092a0ff75
Chris Laprun <metacosm@gmail.com>
2025-11-04 11:07:37 +0100

    docs: update README and add endpoint documentation

--- server/src/main/java/net/laprun/sustainability/power/PowerResource.java
+++ server/src/main/java/net/laprun/sustainability/power/PowerResource.java
@@ -21,6 +21,9 @@ import net.laprun.sustainability.power.persistence.Measure;
 import net.laprun.sustainability.power.persistence.Persistence;
 import net.laprun.sustainability.power.sensors.SamplingMeasurer;
 
+/**
+ * A RESTful endpoint to measure the power consumption associated with processes running on the current system
+ */
 @Path("/power")
 public class PowerResource {
     @Inject
@@ -31,6 +34,12 @@ public class PowerResource {
                 "\nDetected metadata:\n" + metadata());
     }
 
+    /**
+     * Streams power consumption measures associated with the specified process id, if such process is currently running on the system.
+     * @param pid the process identified for which we want to stream measures
+     * @return a stream of {@link SensorMeasure}
+     * @throws Exception if an error occurred while measuring the power consumption
+     */
     @GET
     @RestStreamElementType(MediaType.APPLICATION_JSON)
     @Path("stream/{pid}")
@@ -42,6 +51,12 @@ public class PowerResource {
         }
     }
 
+    /**
+     * Starts measuring the power consumption of the specified application as identified with the specified name and process id
+     * @param appName the application name, used to correlate several measures across different runs
+     * @param pid the process id associated with the current run of the application
+     * @throws Exception if an error occurred while measuring the power consumption
+     */
     @POST
     @Path("start/{appName}/{pid}")
     public void startMeasure(@PathParam("appName") String appName, @PathParam("pid") String pid) throws Exception {
@@ -52,18 +67,31 @@ public class PowerResource {
         }
     }
 
+    /**
+     * Retrieves the metadata associated with the power sensors of the underlying platform
+     * @return the {@link SensorMetadata} associated with the platform's sensors
+     */
     @GET
     @Path("metadata")
     public SensorMetadata metadata() {
         return measurer.metadata();
     }
 
+    /**
+     * Retrieves the currently used sampling period
+     * @return the sampling period
+     */
     @GET
     @Path("sampling")
     public Duration samplingPeriod() {
         return measurer.samplingPeriod();
     }
 
+    /**
+     * Retrieves all recorded measures associated with the specified application
+     * @param appName the application identifier as provided to {@link #startMeasure(String, String)}
+     * @return the chronological list of measures associated with the specified application
+     */
     @GET
     @Path("measures/{appName}")
     public List<SensorMeasure> measures(@PathParam("appName") String appName) {
