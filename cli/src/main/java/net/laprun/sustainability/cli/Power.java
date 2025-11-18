5e898db3045a20b12cfd172c3efabeb338f52b4c
Chris Laprun <metacosm@gmail.com>
2025-11-18 13:17:20 +0100

    chore: release 0.5.0

--- cli/src/main/java/net/laprun/sustainability/cli/Power.java
+++ cli/src/main/java/net/laprun/sustainability/cli/Power.java
@@ -5,7 +5,7 @@
 
 //DEPS io.quarkus:quarkus-bom:${quarkus.version:3.29.3}@pom
 //DEPS io.quarkus:quarkus-picocli
-//DEPS net.laprun.sustainability:power-server-bom:${power-server.version:0.3.1-SNAPSHOT}@pom
+//DEPS net.laprun.sustainability:power-server-bom:${power-server.version:0.5.0}@pom
 //DEPS net.laprun.sustainability:power-server-backend
 //DEPS net.laprun.sustainability:power-server-measure
 
