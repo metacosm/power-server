387747a042444ba85accea78df4ae83299dcb7c7
Chris Laprun <metacosm@gmail.com>
2025-11-21 18:39:41 +0100

    feat: separate sensor / cpu streams more cleanly

--- backend/src/main/java/net/laprun/sustainability/power/sensors/MapMeasures.java
+++ backend/src/main/java/net/laprun/sustainability/power/sensors/MapMeasures.java
@@ -48,4 +48,9 @@ public class MapMeasures implements Measures {
     public SensorMeasure getOrDefault(RegisteredPID pid) {
         return measures.getOrDefault(pid, SensorMeasure.missing);
     }
+
+    @Override
+    public String toString() {
+        return measures.toString();
+    }
 }
