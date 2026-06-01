27d0c5974e4736e44bd555d77fa95f1954bec8ff
Chris Laprun <metacosm@gmail.com>
2026-06-01 14:38:20 +0200

    feat: saving with MockPersistence returns constant Measure

--- server/src/test/java/net/laprun/sustainability/power/MockPersistence.java
+++ server/src/test/java/net/laprun/sustainability/power/MockPersistence.java
@@ -1,8 +1,18 @@
 package net.laprun.sustainability.power;
 
 import io.quarkus.test.Mock;
+import net.laprun.sustainability.power.persistence.Measure;
 import net.laprun.sustainability.power.persistence.Persistence;
 
 @Mock
 public class MockPersistence extends Persistence {
+
+    public static final long ID = 1234L;
+
+    @Override
+    public Measure save(SensorMeasure measure, String appName, String session) {
+        final var m = new Measure();
+        m.id = ID;
+        return m;
+    }
 }
